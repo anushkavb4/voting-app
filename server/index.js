3b9fab130668cd60ae69504e31fbe5a59b8b88d5
anushkavb4 <123955032+anushkavb4@users.noreply.github.com>
2025-03-11 11:58:57 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,17 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const db = require('./models');
+const handle = require('.handlers');
+const { errorHandler } = require('./handlers');
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => res.json({hello: 'world'}));
-app.use((req, res, next) => {
-    const err = new Error('Not found');
-    err.status = 404;
 
-    next(err);
-})
-app.use((err, req, res, next) =>{
-    res.status(err.status || 500).json({
-        err:err.message || 'Something went wrong'
-    })
-}); 
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.use(handle.notFound);
+app.use(handle.errors); 
 app.listen(port, () => console.log(`Server started on port ${port}`));

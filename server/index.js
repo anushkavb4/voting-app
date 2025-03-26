6ada98ff01add9ee026ab9ab5968e3302a64668d
anushkavb4 <123955032+anushkavb4@users.noreply.github.com>
2025-03-26 12:11:59 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -6,11 +6,16 @@ const bodyParser = require('body-parser');
 
 const db = require('./models');
 const handle = require('.handlers');
+const routes = require('./routes');
+
+
+
 const { errorHandler } = require('./handlers');
 const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => res.json({hello: 'world'}));
+app.use('/api/auth', routes.auth);
 
 app.use(cors());
 app.use(bodyParser.json());
--- server/models/index.js
+++ server/models/index.js
@@ -3,3 +3,6 @@ const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/vote');
+
+module.exports.User('./user');
+module.exports.User('./poll');
